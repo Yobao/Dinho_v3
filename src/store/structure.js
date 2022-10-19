4189f13a178b92505bc9beec51e51cd24d5c7238
Yobao <jozef.babos11@gmail.com>
2022-10-19 10:03:33 +0200

    IDK

--- src/store/structure.js
+++ src/store/structure.js
@@ -1,1 +1,16 @@
 export const URL = "https://site--dinhoapi--njvrgqn465n5.code.run/table?club=0";
+
+export const SCORE_TABLE_BODY = [
+	{ name: "position", class: "has-text-weight-bold is-unselectable" },
+	{ name: "username", class: "is-clickable is-unselectable" },
+	{ name: "score", class: "has-background-primary-light is-unselectable" },
+	{ name: "last_round_tip_name", class: "is-unselectable" },
+	{ name: "last_round_score", class: "is-unselectable" },
+];
+
+export const USER_TABLE_BODY = [
+	{ name: "opp_name", class: "is-unselectable" },
+	{ name: "match_start", class: "has-background-primary-light is-unselectable" },
+	{ name: "player_name", class: "is-unselectable" },
+	{ name: "score", class: "is-unselectable" },
+];
