aad1b3806bd248d3dcba4249eb785a91852bfdea
Yobao <jozef.babos11@gmail.com>
2022-11-02 14:09:04 +0100

    feature/scoreTablePage/initial

---
+++ src/components/table.js
@@ -0,0 +1,8 @@
+import React, { useState, useEffect, useContext, useRef } from "react";
+import { CurrentUserContext, OtherUserContext } from "../store/user-context";
+
+const TableComponent = ({}) => {
+	return <table className='table is-bordered is-striped is-hoverable'></table>;
+};
+
+export default TableComponent;
