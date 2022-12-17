f5e1d499c6b8a6627afa38dececacae7121335c7
Jozef Babos <jozef.babos11@gmail.com>
2022-12-17 15:08:05 +0100

    bugfix/hot-fixes/changePwdModal-refactor

--- src/modals/changepwd-modal.js
+++ src/modals/changepwd-modal.js
@@ -1,29 +1,35 @@
-import React, { useState, useContext, useRef, useCallback, useMemo } from "react";
+import React, { useState, useContext, useCallback, useMemo } from "react";
 import ReactDOM from "react-dom";
+import { useNavigate } from "react-router-dom";
 import useFetch from "../hooks/use-fetch";
 import { LanguageContext } from "../store/user-context";
-import { URL } from "../store/data";
 import ModalComponent from "../components/modal";
+import toastik from "../components/ui/toast";
 
 const ChangePwdModal = ({ showModal }) => {
    const { applanguage, setApplanguage } = useContext(LanguageContext);
+   const alerts = applanguage.changePwdModal.warnings;
    const [oldPwd, setOldPwd] = useState();
    const [newPwd, setNewPwd] = useState();
    const [oldPwdColor, setOldPwdColor] = useState();
    const [newPwdColor, setNewPwdColor] = useState();
    const [submitSent, setSubmitSent] = useState(false);
+   const navigate = useNavigate();
 
    const handleOldPwd = useCallback(
       (e) => {
-         setOldPwd(e.target.value);
-         if (submitSent) setOldPwdColor(e.target.value.length < 6 ? "is-danger" : "");
+         const val = e?.target === undefined ? e : e.target.value;
+         console.log(val);
+         setOldPwd(val);
+         if (submitSent) setOldPwdColor(val.length < 6 ? "is-danger" : "");
       },
       [submitSent]
    );
    const handleNewPwd = useCallback(
       (e) => {
-         setNewPwd(e.target.value);
-         if (submitSent) setNewPwdColor(e.target.value.length < 6 ? "is-danger" : "");
+         const val = e?.target === undefined ? e : e.target.value;
+         setNewPwd(val);
+         if (submitSent) setNewPwdColor(val.length < 6 ? "is-danger" : "");
       },
       [submitSent]
    );
@@ -35,6 +41,10 @@ const ChangePwdModal = ({ showModal }) => {
       [submitSent]
    );
 
+   const handleInputColors = {
+      setOldPwdColor,
+      setNewPwdColor,
+   };
    const inputColors = {
       oldPwdColor,
       newPwdColor,
@@ -45,19 +55,46 @@ const ChangePwdModal = ({ showModal }) => {
    };
    const options = {
       method: "POST",
-      undefined,
+      token: true,
       accept: true,
       body: JSON.stringify(userData),
    };
 
-   const { isLoading, err, sendRequest } = useFetch();
+   const { err, sendRequest } = useFetch();
    const transformData = (data) => {
+      if (data === 400) return toastik(alerts.pwdMin);
+      if (data === 409) {
+         setOldPwdColor("is-danger");
+         return toastik(alerts.wrongPassword);
+      }
+      if (data === 500) return toastik(alerts.somethingWrong);
       showModal();
-      window.location.href = `${URL}/`;
+      navigate("/");
    };
 
    const changePassword = useCallback(() => {
       setSubmitSent(true);
+      let message;
+      let index = 0;
+      let checkUserData = {
+         oldPwd,
+         newPwd,
+      };
+
+      // Fill all fields condition.
+      for (const input in checkUserData) {
+         if (checkUserData[input] === null || checkUserData[input] === "") {
+            if (!message) message = alerts.fillEverything;
+            Object.values(handleInputColors)[index]("is-danger");
+         }
+         index++;
+      }
+      if (message) return toastik(message);
+      // Inputfield specific.
+      for (let i = 0; i < Object.keys(handleInputs).length; i++) {
+         Object.values(handleInputs)[i](Object.values(checkUserData)[i]);
+      }
+
       sendRequest("/change_password", options, transformData);
    }, [options.body]);
 
